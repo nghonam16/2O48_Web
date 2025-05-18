9303ab3643ef5a937408e7a7d51479c9f68d3361
Nguyen Nam <nghoofnam@gmail.com>
2025-05-18 13:58:31 +0700

    Update SaveGameState

--- Demo_Game/static/script.js
+++ Demo_Game/static/script.js
@@ -17,3 +17,23 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error(err);
     });
 });
+
+function saveGameState(username, matrix, score) {
+    fetch('/api/save', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            matrix: matrix,
+            score: score
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert(data.reply);
+    })
+    .catch(err => console.error('Lỗi:', err));
+}
+
